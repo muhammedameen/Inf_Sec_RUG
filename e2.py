9a68c82ba7d772f5c845ac66a9bb246cb4909d75
mamameen <cs16b109@smail.iitm.ac.in>
2019-09-13 20:14:17 +0530

    E2 - output and commenting

--- e2.py
+++ e2.py
@@ -1,16 +1,21 @@
 import argparse
 import sys
 
+# Function to encrypt/decrypt an alphabet
 def transform(ch, mapping, alpha):
 	# return chth elemant of mapping
 	i = alpha.find(ch)
 	# print("Mapped ", ch, i, mapping[i])
 	return mapping[i]
 
+# Function to check if the key provided is valid 
 def check_mapping(mapping, alpha):
 	string = ''.join(sorted(list(mapping)))
 	return string == alpha
 
+# Function to encrypt/decrypt a string
+# Mapping - key for encryption
+# alpha - the set of aplphabets in language
 # mode 0 when -o not present , mode 1 when -o present 
 def string_transform(string, mapping, alpha, mode):
 	string1 = ''
@@ -41,6 +46,7 @@ def string_transform(string, mapping, alpha, mode):
 
 def main():
 
+	# checking if all required parameters present
 	parser = argparse.ArgumentParser(description="where:")
 	parser.add_argument("mapping", help = "26 letter char-mappig \n or an int-value")
 	parser.add_argument("-o", help = "keep non-letters as is, honor letter casing", action = "store_true")
@@ -51,19 +57,30 @@ def main():
 	    parser.print_help()
 	    sys.exit(0)
 		
+	# english alphabets
 	alpha = 'abcdefghijklmnopqrstuvwxyz'
 	# mapping = "yxzwvtsqpnrmlkjgfdchbuoaie"
 	# string = "QwertyuioP1234"
 	mapping = args.mapping
+
+	# check if key is a valid mapping 
+	if check_mapping(mapping, alpha) is False:
+		print("Key Invalid")
+		sys.exit(0)
+
+	# setting mode -o
 	if args.o is True:
 		mode_o = 1
 	else:
 		mode_o = 0
+
+	# setting decryption mode
 	if args.d is True:
 		mode_d = 1
 	else:
 		mode_d = 0
 	
+	# opening the file to be decrypted
 	with open('2019.enc', 'r') as myfile:
   		data = myfile.read()
 	

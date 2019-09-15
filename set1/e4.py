2841348cf95d8a672306e1fc4e98d9f03201677d
Ilse <ilse.pubben@gmail.com>
2019-09-15 11:30:56 +0200

    first part of ex4

--- set1/e4.py
+++ set1/e4.py
@@ -1,4 +1,40 @@
 import sys
+import statistics
 
-with open('ex4_schneier.enc', 'r') as myfile:
-  		data = myfile.read()
+def sumStdDeviations(frequencyVectors):
+	stdDev = 0
+	for row in frequencyVectors:
+		stdDev += statistics.stdev(row)
+	
+	return stdDev
+
+
+def getColumnIdx(ch):
+	alphabet = 'abcdefghijklmnopqrstuvwxyz'
+	return alphabet.find(ch)
+
+def getFrequencyVectors(text, keySize) :
+	idx = 0
+	frequencyVectors = [[0 for i in range(26)] for j in range(keySize)] #2D array with keysize rows and 26 columns
+	for ch in text: 
+		row = idx % keySize
+		column = getColumnIdx(ch)
+		frequencyVectors[row][column] += 1
+		idx += 1
+	return frequencyVectors
+	
+
+
+def main() :
+	with open('ex4_schneier.enc', 'r') as myfile:
+			text = myfile.read()
+	
+	for keySize in range(5,16):
+		frequencyVectors = getFrequencyVectors(text, keySize)
+		stdDev = sumStdDeviations(frequencyVectors)
+		print("sum of ", keySize, "std. devs: ", stdDev)
+		# for row in frequencyVectors: 
+		#  	print(row) 
+
+if __name__ == "__main__":
+    main()

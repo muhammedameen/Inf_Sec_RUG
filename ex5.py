4f9773a364db4412a2ecd44829e713cecc266acb
minimi3 <emma.seeger@me.com>
2019-09-28 19:06:04 +0200

    Add files via upload
    Not working. Key must be interpretet as hexadecimals and the Xor function has to be modified

--- ex5.py
+++ ex5.py
@@ -1,4 +1,8 @@
 import hashlib
+# dafuq? hexadecimals, binary, string. when do i need which data type?
+
+
+
 def main() :
 
     #ciphertext
@@ -8,56 +12,62 @@ def main() :
 
     # 2) pass password to sha256
     hash1 = hashlib.sha256(password.encode()).hexdigest()
+    print(type(hash1))
 
     # 3) pass the resulting value again to sha256
-    hash2 = hashlib.sha256(hash1.encode()).hexdigest()
+    hash2 = hashlib.sha256(hash1.encode('UTF-8')).hexdigest()
 
     #make hexadecimal keys
     global wholekey
     wholekey = hash1 + hash2
 
-    plaintext = input("give me input nom, nom: ")
+    plaintext = input("input: ")
     #test if input is right size or use padding
 
+    #split plaintext into blocks of 8 bytes
     # repeat as many times as there is blocks
     for i in range (0, len(plaintext), 8):
         #split input into blocks of 8bytes
         block = plaintext[i:(i+8)]
-        #split block into LH & RH
-        LH = block[0 : len(block)//2]
-        RH = block[len(block)//2 : ]
 
-        #make 16 rounds with rh and lh
-        for i in range (0, 16):
-            LH, RH = feistel(LH,RH,i)
+        #split block into LH & RH
+        LH = block[0 : ((len(block)//2))]
+
+        RH = block[(len(block)//2) : ]
+
+
+        #encrytion
+        # 16 rounds
+        for r in range (0, 16):
+            LH, RH = feistel(LH,RH,r)
+
         #append the ciphertext of the last block
-        ciphertext = ciphertext + feistel(LH,RH,i)
+        ciphertext += (LH + RH)
+
     return ciphertext
 
 #returns the 4byte key of a certain round
 def key(round) :
-    return wholekey[round*8 : (round*8)+4 ]
+    currentkey = wholekey[round*8 : (round*8)+4 ]
+    return currentkey
 
-<<<<<<< HEAD
-# def keyz() :
-
-# def feistel() :
-=======
 def feistel(LH,RH,round) :
 
     oldLH = LH
     # the old right half becomes the new left half
     LH = RH
-
-    # LH1 ^ F(RH, key)
-    # old left half XORed with
-    RH = int(oldLH, base = 16) ^ int(key(round), base = 16)
+    print(oldLH, key(round))
+    RH = xor(oldLH, key(round))
     return(LH, RH)
 
->>>>>>> d1ab48686720602b22c5a42e4730f5b0d3d72ba1
+#hex(ord('a') ^ 0x4b)
+
+# xor two strings
+def xor(s1, s2):
+    return ''.join(chr(ord(a) ^ ord(b)) for a, b in zip(s1, s2))
 
 if __name__ == "__main__":
-    main()
+    print(main())
 
     # 4)keys =  interpret characters of the sha256sum as hexadecimal values. (at the end: 64 bytes)
     # feistel function works on blocks of 8 bytes
